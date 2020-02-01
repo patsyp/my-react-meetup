8126bdecc67de6907e6805c2fbed28d5d266a0a6
Patsy Pineda <patsyp014@gmail.com>
2020-02-01 16:29:30 -0600

    Updates

---
+++ precache-manifest.b68165477c56f91d52c93d1cb1bff559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573986eaba27e5d2ca6268be19ba9a93",
+    "url": "/my-react-meetup/index.html"
+  },
+  {
+    "revision": "139e9bf0d3a3d8c37b1c",
+    "url": "/my-react-meetup/static/js/2.59c3240f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-react-meetup/static/js/2.59c3240f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "255d2775a12fdc64a741",
+    "url": "/my-react-meetup/static/js/main.fabdf585.chunk.js"
+  },
+  {
+    "revision": "b6d2bea484d699215ad7",
+    "url": "/my-react-meetup/static/js/runtime-main.e9ac609b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-react-app/precache-manifest.41bf5e6e796e224d671164965cd54a5d.js"
+  "/my-react-meetup/precache-manifest.b68165477c56f91d52c93d1cb1bff559.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-react-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-react-meetup/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
